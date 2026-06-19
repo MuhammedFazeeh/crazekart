f252ca6a6d968498b72bf67dbcd8c6af4710a1a2
Muhammed Fazeeh <fazeeh.2005@gmail.com>
2026-06-19 12:06:58 +0530

    rename

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import "./products.css";
+import "./Products.css";
 
 function Products() {
   const [products, setProducts] = useState([]);
